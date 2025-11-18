cb41453167ff8944056698f3d858654ca567b554
hanzo7656-prog <hanzo7656@gmail.com>
2025-11-18 18:05:59 +0330

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,6 @@
-import { useEffect } from 'react';
+// pages/_app.js
 import './styles/globals.css';
-import './styles/themes.css';
-import { AppStateProvider } from './hooks/useAppState';
-import NotificationsContainer from './components/UI/NotificationsContainer';
 
-function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    // بررسی پشتیبانی از سرویس ورکر
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('/sw.js')
-        .then((registration) => {
-          console.log('SW registered: ', registration);
-        })
-        .catch((registrationError) => {
-          console.log('SW registration failed: ', registrationError);
-        });
-    }
-  }, []);
-
-  return (
-    <AppStateProvider>
-      <div className="app">
-        <Component {...pageProps} />
-        <NotificationsContainer />
-      </div>
-    </AppStateProvider>
-  );
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp;
